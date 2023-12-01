cc39711fdab7f867314ada1ab5b051a464859bb0
Garett.Janulewicz <janulewiczgarett@gmail.com>
2023-12-01 13:48:56 -1000

    Binary Tree algo

--- 10_Data_Structures/60_Trees/01_BinaryTree.js
+++ 10_Data_Structures/60_Trees/01_BinaryTree.js
@@ -41,13 +41,12 @@ class BinaryTree {
     }
 }
 
-// Example usage:
 const tree = new BinaryTree();
 tree.insert(1);
 tree.insert(2);
 tree.insert(3);
 tree.insert(4);
-// tree.insert(5);
-// tree.insert(6);
+tree.insert(5);
+tree.insert(6);
 
-// console.log(tree);
+console.log(tree);
